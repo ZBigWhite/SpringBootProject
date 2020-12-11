02e67ff6fcb64d8375150a377399a5f775bebc39
ZBigWhite <946473603@qq.com>
2020-12-12 01:40:27 +0800

    test

---
+++ hello-spring/src/main/java/geektime/spring/hello/TestService.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C), 2002-2020, 苏宁易购电子商务有限公司
+ * FileName: TestService.java
+ * Author:   20018848
+ * Date:     2020/12/12 1:39
+ * Description: //模块目的、功能描述
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名    修改时间    版本号       描述
+ */
+package geektime.spring.hello;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 功能描述:<br>
+ *
+ * @author 20018848
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+@Service
+public class TestService {
+}
